d77f946c10a3a651485944ee25fc26e5726d11cd
davirlein1988 <lein.davir@gmail.com>
2019-02-20 01:07:06 -0500

    finished initial user model module

--- models/user.js
+++ models/user.js
@@ -15,3 +15,47 @@ function validateUser(user) {
     return Joi.validate(user, schema);
 }
 
+//user schema
+
+const userSchema = new mongoose.Schema(
+    {
+        firstName: {
+            type: String,
+            required: true,
+            minlength: 3,
+            maxlength: 30
+        },
+        lastName: {
+            type: String,
+            required: true,
+            minlength: 3,
+            maxlength: 30
+        },
+        email: {
+            type: String,
+            required: true,
+            minlength: 10,
+            maxlength: 255,
+            unique: true
+        },
+        password: {
+            type: String,
+            required: true,
+            minlength: 6,
+            maxlength: 1024
+        },
+        role: {
+            type: String,
+            required: true
+        }
+    }
+);
+
+userSchema.methods.generateAuthToken = function () {
+    const token = jwt.sign({_id: this._id, role: this.role}, config.get('jwtPrivateKey'));
+    return token;
+}
+
+const User = mongoose.model('User', userSchema);
+exports.User = User;
+exports.validate = validateUser;
